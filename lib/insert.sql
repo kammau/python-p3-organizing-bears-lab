6dc8bfab28d3da8f99bf50ff32bd89ea2b921c25
Kamryn Smith <programersign@gmail.com>
2023-06-19 10:35:31 -0700

    assignment complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 10, "M", "Brown", "Sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "Dark Blonde", "Crazy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 15, "F", "White", "Calm", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 22, "M", "Black", "Lone wolf", 0);
+INSERT INTO bears VALUES (5, "Melissa", 2, "F", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (6, "Grinch", 7, "M", "Green", "Sneeky", 1);
+INSERT INTO bears VALUES (7, "Wendy", 14, "F", "Black", "Adeventerous", 1);
+INSERT INTO bears VALUES (8, NULL, 12, "F", "Blonde", "Shy", 1);
+
